1e129ce494db52c621ebe530dcd8d5421e88d48a
DevAlexSanz <>
2022-11-19 02:28:57 -0600

    update

--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,7 @@ const navlinks = document.querySelector(".nav-links");
 menuBtn.addEventListener("click", () => {
   navlinks.classList.toggle("mobile-menu");
 });
+
+navlinks.addEventListener("click", () => {
+  navlinks.classList.remove("mobile-menu");
+});
